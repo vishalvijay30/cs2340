8c2511af5145164a4bde0821c9d610883a01af06
vishalvijay30 <sinjaybrothers@gmail.com>
2017-02-17 13:27:03 -0500

    email password recovery

---
+++ app/src/main/java/com/example/vishal/waterreports/ForgotPasswordActivity.java
@@ -0,0 +1,72 @@
+package com.example.vishal.waterreports;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailText;
+    private Button sendEmailButton;
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(ForgotPasswordActivity.this);
+
+        emailText = (EditText) findViewById(R.id.editTextForgotPasswordEmail);
+        sendEmailButton = (Button) findViewById(R.id.buttonReset);
+
+        sendEmailButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == sendEmailButton) {
+            resetPassword();
+        }
+    }
+
+    private void resetPassword() {
+        String email = emailText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            //email is empty, stop execution
+            Toast.makeText(ForgotPasswordActivity.this, "Please enter valid email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Sending email...");
+        progressDialog.show();
+
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.hide();
+                            Toast.makeText(ForgotPasswordActivity.this, "Email Sent!", Toast.LENGTH_LONG).show();
+                        } else {
+                            progressDialog.hide();
+                            Toast.makeText(ForgotPasswordActivity.this, "Invalid Email", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/vishal/waterreports/LoginActivity.java
+++ app/src/main/java/com/example/vishal/waterreports/LoginActivity.java
@@ -26,6 +26,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText editTextPassword;
     private TextView textViewRegister;
     private Button buttonCancel;
+    private TextView textViewForgotPassword;
 
     private ProgressDialog progressDialog;
 
@@ -43,10 +44,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         editTextPassword = (EditText) findViewById(R.id.editTextLoginPassword);
         textViewRegister = (TextView) findViewById(R.id.textViewLoginRegister);
         buttonCancel = (Button) findViewById(R.id.buttonCancel);
+        textViewForgotPassword = (TextView) findViewById(R.id.textViewLoginForgotPassword);
 
         buttonLogin.setOnClickListener(this);
         textViewRegister.setOnClickListener(this);
         buttonCancel.setOnClickListener(this);
+        textViewForgotPassword.setOnClickListener(this);
     }
 
     private void userLogin() {
@@ -55,13 +58,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         if (TextUtils.isEmpty(email)) {
             //email is empty, stop execution
-            Toast.makeText(LoginActivity.this, "Please enter valid email", Toast.LENGTH_SHORT);
+            Toast.makeText(LoginActivity.this, "Please enter valid email", Toast.LENGTH_LONG).show();
             return;
         }
 
         if (TextUtils.isEmpty(password)) {
             //password is empty, stop execution
-            Toast.makeText(LoginActivity.this, "Please enter valid password", Toast.LENGTH_SHORT);
+            Toast.makeText(LoginActivity.this, "Please enter valid password", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -76,7 +79,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             finish();
                             startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
                         } else {
-                            System.out.println("reached unsuccessful login");
                             progressDialog.hide();
                             Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_LONG).show();
                         }
@@ -98,6 +100,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (view == buttonCancel) {
             finish();
             startActivity(new Intent(this, MainActivity.class));
+        }
+
+        if (view == textViewForgotPassword) {
+            finish();
+            startActivity(new Intent(this, ForgotPasswordActivity.class));
         }
     }
 }
